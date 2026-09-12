db60fc8e6129483e3223169b74866c87a5dcc11b
GeorgiPanaiotov <rippness@gmail.com>
2026-09-12 21:17:27 +0300

    some design changes and added initial drag and drop

--- build/index.js
+++ build/index.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log("Hello, World!");
+const dropZone = document.getElementById("drop-zone");
+const fileInput = document.getElementById("file-input");
+dropZone?.addEventListener("click", () => fileInput?.click());
+fileInput?.addEventListener("change", (e) => handleFile(e?.target?.files[0]));
+dropZone?.addEventListener("dragover", (e) => e.preventDefault());
+dropZone?.addEventListener("drop", (e) => {
+    e.preventDefault();
+    if ((e?.dataTransfer).files.length)
+        handleFile((e?.dataTransfer).files[0]);
+});
+function handleFile(file) {
+    if (!file)
+        return;
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = (e) => parseCustomTextFormat((e?.target).result);
+}
+function parseCustomTextFormat(bytes) {
+    console.log("Raw File Bytes Loaded:", bytes);
+}
--- ts/index.ts
+++ ts/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello, World!");
+const dropZone = document.getElementById("drop-zone");
+const fileInput = document.getElementById("file-input");
+
+dropZone?.addEventListener("click", () => fileInput?.click());
+
+fileInput?.addEventListener("change", (e) => handleFile((e?.target as any)?.files[0]));
+
+dropZone?.addEventListener("dragover", (e) => e.preventDefault());
+dropZone?.addEventListener("drop", (e) => {
+  e.preventDefault();
+  if ((e?.dataTransfer as any).files.length) handleFile((e?.dataTransfer as any).files[0]);
+});
+
+function handleFile(file: Blob) {
+  if (!file) return;
+
+  const reader = new FileReader();
+
+  reader.readAsText(file);
+  reader.onload = (e) => parseCustomTextFormat((e?.target as any).result);
+}
+
+function parseCustomTextFormat(bytes: string | ArrayBuffer | null) {
+  console.log("Raw File Bytes Loaded:", bytes);
+}
